9de4e164a91681698b7da44b405546ab5261b042
Sandrro <antonov-as@yandex.ru>
2024-11-29 15:56:12 +0300

    refactor(RiskCalculation): made input parameters obligatory in DTO

--- app/risk_calculation/dto/project_territory_dto.py
+++ app/risk_calculation/dto/project_territory_dto.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 from geojson_pydantic import MultiPolygon, Polygon
 from app.risk_calculation.dto._polygon_example import poly
 
@@ -7,5 +6,5 @@ class ProjectTerritoryRequest(BaseModel):
     """
     Class for post request by territory id within the specified project territory.
     """
-    territory_id: Optional[int] = Field(default=None, examples=[1])
-    selection_zone: Optional[Polygon | MultiPolygon] = Field(default=None, examples=[poly])
+    territory_id: int = Field(..., examples=[1])
+    selection_zone: Polygon | MultiPolygon = Field(..., examples=[poly])
