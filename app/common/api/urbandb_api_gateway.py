db9749915b2aaa0104d38b812ef1a693de03e890
Sandrro <antonov-as@yandex.ru>
2025-01-17 17:15:34 +0300

    feat: changed clip territory to context, changed routers to get

--- app/common/api/urbandb_api_gateway.py
+++ app/common/api/urbandb_api_gateway.py
@@ -1,7 +1,7 @@
 import aiohttp
 from loguru import logger
 import json
-from shapely.geometry import shape
+from shapely.geometry import shape, Point
 import geopandas as gpd
 import pandas as pd
 
@@ -33,7 +33,7 @@ class UrbanDBAPI:
                     logger.error(f"Failed to fetch child territories, status code: {response.status}")
                     raise http_exception(404, f"Failed to fetch child territories", response.status)
 
-    async def get_parent_territory(self, territory_id: int):
+    async def get_territory(self, territory_id: int):
         """
         Collecting parent territory from UrbanDB_API.
         """
@@ -49,17 +49,65 @@ class UrbanDBAPI:
                     territory = gpd.GeoDataFrame(pd.DataFrame([territory], columns = ['name', 'geometry']), geometry='geometry', crs=4326) 
                     return territory
                 else:
-                    logger.error(f"Failed to fetch parent territory, status code: {response.status}")
-                    raise http_exception(404, f"Failed to fetch parent territory", response.status)
+                    logger.error(f"Failed to fetch territory, status code: {response.status}")
+                    raise http_exception(404, f"Failed to fetch territory", response.status)
 
     async def get_territories(self, territory_id):
         """
         Collecting all territories from UrbanDB_API
         """
-        parent_territory = await self.get_parent_territory(territory_id)
+        parent_territory = await self.get_territory(territory_id)
         child_territories = await self.get_child_territories(territory_id, 1, 10000)
         territories = pd.concat([parent_territory, child_territories])
         territories = territories.explode().reset_index(drop=True).drop_duplicates(subset='name')
-        return territories          
+        return territories
 
+    async def get_project_territory_centroid(self, project_id):
+        """
+        Fetching centroid of territory of the project
+        """
+        api_url = f"{self.url}/v1/projects/{project_id}/territory"
+        logger.info(f"Fetching project territory from API: {api_url}")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as response:
+                if response.status == 200:
+                    json_data = await response.json()
+                    centre_coords = json_data.get("centre_point", {}).get("coordinates", None)
+                    return Point(centre_coords)
+                else:
+                    logger.error(f"Failed to fetch centroid of territory, status code: {response.status}")
+                    raise http_exception(404, f"Failed to fetch centroid of territory", response.status)
+
+    async def get_context_ids(self, territory_id, project_id):
+        """
+        Finding id of terrotories which are in the context of the project
+        """
+        api_url = f"{self.url}/v1/projects?is_regional=false&territory_id={territory_id}&page=1&page_size=10000"          
+        logger.info(f"Fetching list of projects for the territory from API: {api_url}")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as response:
+                if response.status == 200:
+                    json_data = await response.json()
+                    logger.info(f"List of projects for the territory {territory_id} successfully fetched from API.")
+                    results = json_data.get("results", [])
+                    for project in results:
+                        if project.get("project_id") == project_id:
+                            return project.get("properties", {}).get("context", [])
+                    raise http_exception(404, f"Failed to fetch list of context ids", response.status)
+                else:
+                    logger.error(f"Failed to fetch list of projects, status code: {response.status}")
+                    raise http_exception(404, f"Failed to fetch list of projects", response.status)
+
+    async def get_context_territories(self, territory_id, project_id):
+        """
+        Collecting parent territory from UrbanDB_API.
+        """
+        context_list = await self.get_context_ids(territory_id, project_id)
+        context_territories = [await self.get_territory(territory_id) for context_territory in context_list]
+        context_territories = pd.concat(context_territories)
+        logger.info(f"Context territories for project {project_id} succesfully collected")
+        return context_territories
+                
 urban_db_api = UrbanDBAPI()
--- app/risk_calculation/social_risk_controller.py
+++ app/risk_calculation/social_risk_controller.py
@@ -2,53 +2,65 @@
 Router module provides api with the api router for service in swagger interface
 and collects clear logic for them.
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from loguru import logger
-from app.risk_calculation.dto.project_territory_dto import ProjectTerritoryRequest
 from app.risk_calculation.logic.spatial_methods import risk_calculator
 from app.risk_calculation.logic.constants import TEXTS, bucket_name, text_name
-
 
 router = APIRouter()
 
 
-@router.post("/social_risk/")
-async def get_social_risk(params: ProjectTerritoryRequest) -> dict[str, dict | list | int]:
+@router.get("/social_risk/")
+async def get_social_risk(
+    territory_id: int = Query(..., description="ID of the territory"),
+    project_id: int = Query(..., description="ID of the project")
+) -> dict[str, dict | list | int]:
     """Function for calculating social risk for the territory
     Args:
-        params (ProjectTerritoryRequest): request in json format from user
+        territory_id (int): ID of the territory
+        project_id (int): ID of the project
     Returns:
         dict: table with social risk data
     """
-    logger.info(f"Started request processing with params{params.__dict__}")
+    logger.info(f"Started request processing with territory_id={territory_id}, project_id={project_id}")
     TEXTS.try_init(bucket_name, text_name)
-    response = await risk_calculator.calculate_social_risk(params.selection_zone)
+    response = await risk_calculator.calculate_social_risk(territory_id, project_id)
     return response
 
-@router.post("/risk_coverage_areas/")
-async def get_social_risk_coverage(params: ProjectTerritoryRequest) -> dict[str, dict | list | int]:
+
+@router.get("/risk_coverage_areas/")
+async def get_social_risk_coverage(
+    territory_id: int = Query(..., description="ID of the territory"),
+    project_id: int = Query(..., description="ID of the project")
+) -> dict[str, dict | list | int]:
     """Function for getting outrage coverage for the territory
     Args:
-        params (ProjectTerritoryRequest): request in json format from user
+        territory_id (int): ID of the territory
+        project_id (int): ID of the project
     Returns:
         dict: dict with two geojsons with coverage areas
     """
-    logger.info(f"Started request processing with params{params.__dict__}")
+    logger.info(f"Started request processing with territory_id={territory_id}, project_id={project_id}")
     TEXTS.try_init(bucket_name, text_name)
-    response = await risk_calculator.calculate_coverage(params.selection_zone, params.territory_id) 
-    logger.info(f"Social risk coverage response generated")
+    response = await risk_calculator.calculate_coverage(territory_id, project_id)
+    logger.info("Social risk coverage response generated")
     return response
 
-@router.post("/collect_texts/")
-async def get_texts_for_territory(params: ProjectTerritoryRequest) -> dict[str, dict | list | int]:
+
+@router.get("/collect_texts/")
+async def get_texts_for_territory(
+    territory_id: int = Query(..., description="ID of the territory"),
+    project_id: int = Query(..., description="ID of the project")
+) -> dict[str, dict | list | int]:
     """Function to collect texts for the territory
     Args:
-        params (ProjectTerritoryRequest): request in json format from user
+        territory_id (int): ID of the territory
+        project_id (int): ID of the project
     Returns:
         dict: dict with dataframe with texts and their attributes
     """
-    logger.info(f"Started request processing with params{params.__dict__}")
+    logger.info(f"Started request processing with territory_id={territory_id}, project_id={project_id}")
     TEXTS.try_init(bucket_name, text_name)
-    response = await risk_calculator.collect_texts(params.selection_zone) 
-    logger.info(f"Texts for social risk collected")
+    response = await risk_calculator.collect_texts(territory_id, project_id)
+    logger.info("Texts for social risk collected")
     return response
